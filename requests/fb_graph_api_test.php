8e194b9089993d3af20ff3e3f4e8556b5a4876ea
Kyle Oswald <kyleleeswald@gmail.com>
2013-09-21 04:49:05 -0400

    Nothing really

---
+++ requests/fb_graph_api_test.php
@@ -0,0 +1,20 @@
+<?php # Kyle Oswald 09/21/2013
+# This is test code to determine the power of the graph api
+
+require_once('/../secure/error_reporting.inc.php');
+require_once('/../secure/site_constants.inc.php');
+require_once('lib/facebook/facebook.php');
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Facebook Graph API Test</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ secure/site_constants.inc.php
@@ -0,0 +1,7 @@
+<?php # Kyle Oswald 09/21/2013
+# Contains various constants such as api keys
+
+DEFINE('FACEBOOK_APP_ID', '1417023041846832');
+DEFINE('FACEBOOK_APP_SECRET', '46e1a2e04fd1f7ab4cb0d3b8023fadf2');
+
+?>
