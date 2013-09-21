ed2b0ed089bbb6f5e20d66a1951500d903f998a3
Matthew Herbst <herbstmb@miamioh.edu>
2013-09-21 01:52:15 -0400

    added basic web files

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+/*
+	Footer to be included on all pages
+*/
+
+//Basic footer tags
+echo "</body></html>";
+
+?>
---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+/*
+	Header to be included on all pages.
+*/
+
+//Basic header tags
+echo "<!DOCTYPE html><html lang='en'><head>";
+
+//Basic header info
+echo "<meta name='author' content='Matthew Herbst'>
+		<meta charset='utf-8'>
+		<title>MHacks</title>";
+
+//Global imports
+echo "<link rel='stylesheet' type='text/css' href='styles/styles.css'>
+		<script type='text/javascript' src='js/d3/d3.v3.js'></script>
+		<script type='text/javascript' src='js/jquery-1.10.2.min.js'></script>";
+		
+//Close header tags
+echo "</head>";
+?>
