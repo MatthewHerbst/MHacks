984a00a5d64ca165c89bca1ba71c93fab68d9d27
Matthew Herbst <herbstmb@miamioh.edu>
2013-09-21 06:34:24 -0400

    added container class to vis div + some styles

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ include "header.php";
     </div>
 </div>
 
-<div id='vis'>Visualization will go here! Please work!</div>
+<div class='container' id='vis'>Visualization will go here! Please work!</div>
 
 <?php
 include "footer.php";
